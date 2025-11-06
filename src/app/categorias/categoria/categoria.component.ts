9aae5fd1e83ce7e327300932ff65c3d5eeed8834
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-11-06 19:07:03 -0300

    feat: adding checker to show span message and when save the category

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators} from'@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-categoria',
   standalone: false,
   templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.scss'
+  styleUrl: './categoria.component.scss',
 })
 export class CategoriaComponent {
   camposForm: FormGroup;
-  constructor()
-  {
-    this.camposForm = new FormGroup(
-      {
-        nome: new FormControl('',Validators.required),
-        descricao: new FormControl('',Validators.required)
-      }
-    );
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    });
   }
 
-  salvar()
-  {
-    console.log('valores digitados', this.camposForm.value)
-    console.log('Está valido ?', this.camposForm.valid)
+  salvar() {
+    this.camposForm.markAllAsTouched();
+    if (this.camposForm.valid) {
+      console.log('valores digitados', this.camposForm.value);
+      console.log('Está valido ?', this.camposForm.valid);
+    }
+  }
+
+  CheckCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return (
+      (campo?.invalid && campo.touched && campo?.errors?.['required']) || false
+    );
   }
 }
