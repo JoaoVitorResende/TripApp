91930499d33c4f523c182bf21f5db61d0875ab0a
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-11-11 19:44:37 -0300

    chore: creating web page

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
 @Component({
   selector: 'app-lugar',
   standalone: false,
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './lugar.component.scss'
 })
 export class LugarComponent {
-
+ camposForm: FormGroup;
+ categorias: Categoria[] = [];
+ constructor()
+ {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('',Validators.required),
+      categoria: new FormControl('',Validators.required),
+      localizacao: new FormControl('',Validators.required),
+      urlFoto: new FormControl('',Validators.required),
+      avaliacao: new FormControl('',Validators.required)
+    });
+ }
+ salvar()
+ {
+    console.log(this.camposForm.value);
+ }
 }
