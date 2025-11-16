22370c8daaf9f5a06c2cf38af2c7628805009a0a
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-11-16 09:52:38 -0300

    chore: adding validations to places page

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -7,36 +7,45 @@ import { LugarService } from '../lugar.service';
   selector: 'app-lugar',
   standalone: false,
   templateUrl: './lugar.component.html',
-  styleUrl: './lugar.component.scss'
+  styleUrl: './lugar.component.scss',
 })
 export class LugarComponent implements OnInit {
- camposForm: FormGroup;
- listaCategorias: Categoria[] = [];
- constructor(private categoriaService: CategoriaService, private lugarService: LugarService)
- {
+  camposForm: FormGroup;
+  listaCategorias: Categoria[] = [];
+  constructor(
+    private categoriaService: CategoriaService,
+    private lugarService: LugarService
+  ) {
     this.camposForm = new FormGroup({
-      nome: new FormControl('',Validators.required),
-      categoria: new FormControl('',Validators.required),
-      localizacao: new FormControl('',Validators.required),
-      urlFoto: new FormControl('',Validators.required),
-      avaliacao: new FormControl('',Validators.required)
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
     });
- }
- ngOnInit(): void {
-   this.categoriaService.GetTodas().subscribe(
-    {
-      next:(categorias) => this.listaCategorias = categorias,
-      error: erro => console.error("não foi possivel pegar categorias", erro)
+  }
+  ngOnInit(): void {
+    this.categoriaService.GetTodas().subscribe({
+      next: (categorias) => (this.listaCategorias = categorias),
+      error: (erro) => console.error('não foi possivel pegar categorias', erro),
+    });
+  }
+  salvar() {
+    this.camposForm.markAllAsTouched();
+    if (this.camposForm.valid) {
+      this.lugarService.register(this.camposForm.value).subscribe({
+        next: (lugar) => {
+          console.log('salvo com sucesso', lugar);
+          this.camposForm.reset();
+        },
+        error: (erro) => console.log('algum erro ocorreu', erro),
+      });
     }
-   );
- }
- salvar()
- {
-   this.lugarService.register(this.camposForm.value).subscribe({
-    next: (lugar) => { console.log('salvo com sucesso', lugar);
-      this.camposForm.reset();
-    },
-    error: erro => console.log('algum erro ocorreu', erro)
-   });
- }
+  }
+  CheckCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return (
+      (campo?.invalid && campo.touched && campo?.errors?.['required']) || false
+    );
+  }
 }
