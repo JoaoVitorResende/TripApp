608cf04b4b8085b2175f32907ee51fc850e659c9
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-11-17 21:14:45 -0300

    feat: creating galeria page

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../../categorias/categoria';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { Lugar } from '../../lugares/lugar';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +10,14 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
-
+export class GaleriaComponent implements OnInit{
+  lugares: Lugar[] = [];
+  categorias: Categoria[] = [];
+  constructor(private  lugarService: LugarService, private categoriaService: CategoriaService)
+  {}
+  ngOnInit(): void {
+    this.categoriaService.GetTodas().subscribe(catego => this.categorias = catego);
+    this.lugarService.GetTodas().subscribe(lug => this.lugares = lug);
+    console.log(this.lugares[2].urlFoto);
+  }
 }
--- src/app/lugares/lugar.ts
+++ src/app/lugares/lugar.ts
@@ -3,5 +3,5 @@ export class Lugar {
     categoria?:string;
     localizacao?:string;
     urlFoto?:string;
-    avaaliacao?: number;
+    avaliacao?: number;
 }
